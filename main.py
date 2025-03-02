c4c5caf805240e4586a63f3f52e4653873f0beaa
asubhekar <subhuatharva@gmail.com>
2025-03-02 17:01:51 -0500

    update app

--- main.py
+++ main.py
@@ -76,4 +76,4 @@ async def inference(request: Request, model_name: str = Form(...), prompt: str =
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
